e4e9517238bfb39c7c15fd715e8ad6c8bf107f88
Nicholas Kurtz <demonintherough@gmail.com>
2018-05-23 09:02:13 -0700

    Removed duplicate constant

--- client/src/Constant.js
+++ client/src/Constant.js
@@ -1,5 +1,4 @@
 export const ADD_INTEREST = "ADD_INTEREST";
 export const ADD_USER = "ADD_USER";
-export const ADD_INTEREST = "ADD_INTEREST"
 export const UPDATE_INTEREST = "UPDATE_INTEREST"
 export const UPDATE_USER = "UPDATE_USER"
