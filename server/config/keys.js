cfea295db89b2d6103c8ed73c094d4e6edae7114
Pocky Jocky <coasterfantom@gmail.com>
2018-07-16 11:23:45 -0400

    Added the 7IS to squadron options and switched mongo server key

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,6 @@
 const keys = {
-    mongoURI: process.env.MONGODB_URL || 'mongodb://tituser:tituser@ds151008.mlab.com:51008/tituserdevdb'
+    // mongoURI: process.env.MONGODB_URL || 'mongodb://tituser:tituser@ds151008.mlab.com:51008/tituserdevdb'
+    mongoURI: process.env.MONGODB_URL || 'mongodb://tituser:tituser@ds117509.mlab.com:17509/talentidproduction'
 };
 
 export default keys
